d1518992903fa0ec459e61e5b2c71740ed84ff18
jangji <kimwlgus7@naver.com>
2018-05-03 17:01:52 +0900

    asdsa
    dsad

--- Zombiesurvive/Assets/02.Script/P92.cs
+++ Zombiesurvive/Assets/02.Script/P92.cs
@@ -5,14 +5,28 @@ using UnityEngine;
 public class P92 : MonoBehaviour {
     public int ShotDamage = 5;
     public float ShotDelay = 0.5f;
+    public float Range = 10f;
+    Z_Faster Z_FasterHealth;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        Debug.DrawRay(transform.position, this.transform.forward * Range, Color.red);
+        if (Input.GetKey("Fire1"))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.forward, out hit, Range))
+            {
+                if (hit.collider.gameObject.tag == "Enemy")
+                {
+                    Debug.Log(hit.collider.name);
+                }
+            }
+
+        }
 	}
 }
